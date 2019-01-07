9b8e6dde4755234b5cbc5c312c5c0ab3efa09e8d
EJLin <ej@creatop.com.tw>
2019-01-07 16:42:02 +0800

    add laradock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Success!!
                 </div>
 
                 <div class="links">
